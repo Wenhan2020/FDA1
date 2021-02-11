ce89b56a7a4ef932b0898e84bf55dfd822eb0d4f
wenhanzhang <zwh.hfut@outlook.com>
2021-02-10 18:35:44 -0700

    Updated the segment function

--- seg.m
+++ seg.m
@@ -2,7 +2,8 @@ function segments = seg(sequence, L)
 % Use the input sequence and defined length to segment the long sequence
 % into short ones. The return value is a 2-D matrix. Each row element is
 % the equal-length segments
-num = length(sequence)/ L;
+%num = floor(length(sequence)/ L);
+num = length(sequence) - L;
 segments = zeros(num, L);
 for i = 1: num
     segments(i,:) = sequence(i:i+L-1);
